a1257b6855bc58fbefd9436d6895256cc2ebeff7
agupta100 <aarav.gupta@menloschool.org>
2023-04-27 07:26:33 -0700

    two images appear on screen, mouse not working yet

--- src/GameView.java
+++ src/GameView.java
@@ -6,13 +6,19 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 public class GameView extends JFrame
+        implements MouseListener, MouseMotionListener
 {
     private static final int
             WINDOW_WIDTH = 800,
             WINDOW_HEIGHT = 600;
     private Image image;
+    private int xMouse;
+    private int yMouse;
     public GameView(Image image)
     {
         this.image = image;
@@ -20,11 +26,40 @@ public class GameView extends JFrame
         this.setTitle("Wii Tennis");
         this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         this.setVisible(true);
+//        window.addMouseListener(window);
+//        window.addMouseMotionListener(window);
     }
 
     public void paint(Graphics g)
     {
         g.drawImage(image, 300, 475, 100, 100, this);
+        g.drawImage(image, xMouse, yMouse, 100, 100, this);
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    public void mouseMoved(MouseEvent e)
+    {
+        // find out where the mouse is
+         xMouse = e.getX();
+         yMouse = e.getY();
+
+        // update the location of the circle so that it follows the mouse
+
+
+        // in your code, do some calculation
+        // example:
+        // ship.updateCannonAngle(xMouse, yMouse);
+        // ship.updateX(xMouse);
+
+
+        // update the screen
+        repaint();
+
+
     }
 
     public Image getImage()
@@ -33,4 +68,28 @@ public class GameView extends JFrame
     }
 
 
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
